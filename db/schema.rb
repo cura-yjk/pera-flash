157f5ef4b2f0678a0a2f82ac77a14a70c84657fa
Matthew Hardcastle <mhtenshokumail@gmail.com>
2026-08-18 16:45:44 +0900

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_18_012035) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_18_064132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_18_012035) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|

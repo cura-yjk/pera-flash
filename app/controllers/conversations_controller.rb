bbbaad603c0ba7b740f4b7c73d6855d896c3944f
Twinky Hung <hungchingmantwinky@gmail.com>
2026-08-18 12:06:49 +0900

    done create method in conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,10 @@
 class ConversationsController < ApplicationController
+  def create
+    @conversation = Conversation.new
+    if @conversation.save
+      redirect_to conversation_path(@conversation)
+    else
+      render "pages/home", status: :unprocessable_entity
+    end
+  end
 end

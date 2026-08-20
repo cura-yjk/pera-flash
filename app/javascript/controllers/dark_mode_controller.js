c285c06c074ae1909040c0b402b0e71ede97c2c4
Matthew Hardcastle <mhtenshokumail@gmail.com>
2026-08-20 11:32:16 +0900

    buttons landing styled

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["dark-toggle", "dark-dash"];
+  static targets = ["dark-toggle", "dark-dash",'rotate-icon'];
 
   connect () {
   const savedTheme = localStorage.getItem("theme");

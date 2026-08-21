a9a7ed9ce485b2fe2adaaa959a18e84d323d110f
Yusuke Kamihanawa <codingpurpose@gmail.com>
2026-08-21 15:04:54 +0900

    added some seed

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,6 @@
 class Deck < ApplicationRecord
   belongs_to :user
-  has_many :flashcards, dependent: :nullify
+  has_many :flashcards, dependent: :destroy
 
   validates :name, presence: true
 end
